8a09bedc7f106bfbd10692a2eed453d4b00a7e65
DarkMentat <indraua@gmail.com>
2016-04-29 21:00:49 +0300

    RecordDecoder class

--- app/src/main/java/org/darkmentat/draftrecorder/media/Player.java
+++ app/src/main/java/org/darkmentat/draftrecorder/media/Player.java
@@ -23,146 +23,17 @@ public class Player {
     void onPlayingStop();
   }
 
-  public static final long TIMEOUT_US = 1000;
-
-  private WeakReference<PlayerListener> mPlayerListener;
-
-  private MediaPlayer mMediaPlayer;
-
   private static AudioTrack getAudioTrack(){
     int minSize = AudioTrack.getMinBufferSize( 16000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
     return new AudioTrack(AudioManager.STREAM_MUSIC, 16000, AudioFormat.CHANNEL_OUT_MONO,
         AudioFormat.ENCODING_PCM_16BIT, minSize, AudioTrack.MODE_STREAM);
   }
-  private static MediaCodec getCodec(MediaExtractor extractor) throws IOException {
-    // Read track header
-    MediaFormat format = null;
-    String mime = "audio/mp4a-latm";
-    int sampleRate = 16000;
-    int channels = 1;
-    int bitrate = 0;
-    long duration = 0;
 
-    format = extractor.getTrackFormat(0);
-    mime = format.getString(MediaFormat.KEY_MIME);
-    sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
-    channels = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
-    duration = format.getLong(MediaFormat.KEY_DURATION);
+  private WeakReference<PlayerListener> mPlayerListener;
 
-    MediaCodec codec = MediaCodec.createDecoderByType(mime);
+  private MediaPlayer mMediaPlayer;
 
-    codec.configure(format, null, null, 0);
-
-    return codec;
-  }
-
-  private MediaExtractor mExtractor;
-  private MediaCodec mCodec;
-  private ByteBuffer[] mCodecInputBuffers;
-  private ByteBuffer[] mCodecOutputBuffers;
-  private MediaCodec.BufferInfo mMediaCodecInfo;
-  private boolean mSawInputEOS;
-  private boolean mSawOutputEOS;
-  private int mNoOutputCounter;
-  private int mNoOutputCounterLimit;
-
-  private long mPresentationTimeUs;
-
-  private void startRecordReading(MusicComposition.Record record){
-    try{
-      mExtractor = new MediaExtractor();
-      mExtractor.setDataSource(record.getFile().getAbsolutePath());
-      mExtractor.selectTrack(0);
-
-      mCodec = getCodec(mExtractor);
-      mCodec.start();
-
-      mCodecInputBuffers = mCodec.getInputBuffers();
-      mCodecOutputBuffers = mCodec.getOutputBuffers();
-
-      mSawInputEOS = false;
-      mSawOutputEOS = false;
-
-      mPresentationTimeUs = 0;
-      mNoOutputCounter = 0;
-      mNoOutputCounterLimit = 10;
-
-      mMediaCodecInfo = new MediaCodec.BufferInfo();
-    }catch(IOException e){
-      e.printStackTrace();
-    }
-  }
-  private byte[] readRecordChunk(){
-
-    if(mSawOutputEOS)
-      return null;
-
-    if(mNoOutputCounter >= mNoOutputCounterLimit)
-      return null;
-
-    mNoOutputCounter++;
-
-    int inputBufIndex = mCodec.dequeueInputBuffer(TIMEOUT_US);
-    if (inputBufIndex >= 0) {
-      ByteBuffer dstBuf = mCodecInputBuffers[inputBufIndex];
-      int sampleSize = mExtractor.readSampleData(dstBuf, 0);
-      if (sampleSize < 0) {
-        mSawInputEOS = true;
-        sampleSize = 0;
-      } else {
-        mPresentationTimeUs = mExtractor.getSampleTime();
-      }
-
-      mCodec.queueInputBuffer(inputBufIndex, 0, sampleSize, mPresentationTimeUs, mSawInputEOS ? MediaCodec.BUFFER_FLAG_END_OF_STREAM : 0);
-
-      if (!mSawInputEOS) mExtractor.advance();
-    }
-
-    // decode to PCM and return it
-    int res = mCodec.dequeueOutputBuffer(mMediaCodecInfo, TIMEOUT_US);
-
-    byte[] chunk = new byte[mMediaCodecInfo.size];
-
-    if (res >= 0) {
-      if (mMediaCodecInfo.size > 0)  mNoOutputCounter = 0;
-
-      int outputBufIndex = res;
-      ByteBuffer buf = mCodecOutputBuffers[outputBufIndex];
-      buf.get(chunk);
-      buf.clear();
-
-      if(chunk.length == 0)
-        chunk = null;
-
-      mCodec.releaseOutputBuffer(outputBufIndex, false);
-      if ((mMediaCodecInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
-        mSawOutputEOS = true;
-      }
-    }
-
-    return chunk;
-  }
-  private void stopRecordReading(){
-    mCodec.stop();
-    mCodec.release();
-
-    mExtractor = null;
-
-    mCodec = null;
-
-    mCodecInputBuffers = null;
-    mCodecOutputBuffers = null;
-
-    mSawInputEOS = false;
-    mSawOutputEOS = false;
-
-    mPresentationTimeUs = 0;
-
-    mNoOutputCounter = 0;
-    mNoOutputCounterLimit = 0;
-
-    mMediaCodecInfo = null;
-  }
+  private boolean mStop = false;
 
   @Background
   public void playStart(MusicComposition composition) {
@@ -172,12 +43,12 @@ public class Player {
 
     MusicComposition.Record record = composition.getRegions().get(0).getTracks().get(0).getRecords().get(0);
 
-    boolean stop = false;
+    RecordDecoder recordDecoder = new RecordDecoder(record);
 
-    startRecordReading(record);
+    recordDecoder.startRecordReading();
 
-    while (!stop) {
-      byte[] chunk = readRecordChunk();
+    while (!mStop) {
+      byte[] chunk = recordDecoder.readRecordChunk();
 
       if(chunk == null)
         break;
@@ -185,14 +56,12 @@ public class Player {
       audioTrack.write(chunk,0,chunk.length);
     }
 
-    stopRecordReading();
+    recordDecoder.stopRecordReading();
 
     audioTrack.flush();
     audioTrack.release();
 
     notifyListenerStop();
-
-    stop = true;
   }
 
   public void playStart(String file) {
@@ -215,6 +84,8 @@ public class Player {
     }
   }
   public void playStop() {
+    mStop = true;
+
     if(mMediaPlayer != null && mMediaPlayer.isPlaying()){
       mMediaPlayer.stop();
 
---
+++ app/src/main/java/org/darkmentat/draftrecorder/media/RecordDecoder.java
@@ -0,0 +1,149 @@
+package org.darkmentat.draftrecorder.media;
+
+import android.media.MediaCodec;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
+
+import org.darkmentat.draftrecorder.domain.MusicComposition;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public class RecordDecoder {
+  private static final long TIMEOUT_US = 1000;
+
+  private static MediaCodec getCodec(MediaExtractor extractor) throws IOException {
+    // Read track header
+    MediaFormat format = null;
+    String mime = "audio/mp4a-latm";
+    int sampleRate = 16000;
+    int channels = 1;
+    int bitrate = 0;
+    long duration = 0;
+
+    format = extractor.getTrackFormat(0);
+    mime = format.getString(MediaFormat.KEY_MIME);
+    sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+    channels = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+    duration = format.getLong(MediaFormat.KEY_DURATION);
+
+    MediaCodec codec = MediaCodec.createDecoderByType(mime);
+
+    codec.configure(format, null, null, 0);
+
+    return codec;
+  }
+
+  private MediaExtractor mExtractor;
+  private MediaCodec mCodec;
+  private ByteBuffer[] mCodecInputBuffers;
+  private ByteBuffer[] mCodecOutputBuffers;
+  private MediaCodec.BufferInfo mMediaCodecInfo;
+  private boolean mSawInputEOS;
+  private boolean mSawOutputEOS;
+  private int mNoOutputCounter;
+  private int mNoOutputCounterLimit;
+
+  private long mPresentationTimeUs;
+  private MusicComposition.Record mRecord;
+
+  public RecordDecoder(MusicComposition.Record record) {
+    mRecord = record;
+  }
+
+  public void startRecordReading(){
+    try{
+      mExtractor = new MediaExtractor();
+      mExtractor.setDataSource(mRecord.getFile().getAbsolutePath());
+      mExtractor.selectTrack(0);
+
+      mCodec = getCodec(mExtractor);
+      mCodec.start();
+
+      mCodecInputBuffers = mCodec.getInputBuffers();
+      mCodecOutputBuffers = mCodec.getOutputBuffers();
+
+      mSawInputEOS = false;
+      mSawOutputEOS = false;
+
+      mPresentationTimeUs = 0;
+      mNoOutputCounter = 0;
+      mNoOutputCounterLimit = 10;
+
+      mMediaCodecInfo = new MediaCodec.BufferInfo();
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
+  public byte[] readRecordChunk(){
+
+    if(mSawOutputEOS)
+      return null;
+
+    if(mNoOutputCounter >= mNoOutputCounterLimit)
+      return null;
+
+    mNoOutputCounter++;
+
+    int inputBufIndex = mCodec.dequeueInputBuffer(TIMEOUT_US);
+    if (inputBufIndex >= 0) {
+      ByteBuffer dstBuf = mCodecInputBuffers[inputBufIndex];
+      int sampleSize = mExtractor.readSampleData(dstBuf, 0);
+      if (sampleSize < 0) {
+        mSawInputEOS = true;
+        sampleSize = 0;
+      } else {
+        mPresentationTimeUs = mExtractor.getSampleTime();
+      }
+
+      mCodec.queueInputBuffer(inputBufIndex, 0, sampleSize, mPresentationTimeUs, mSawInputEOS ? MediaCodec.BUFFER_FLAG_END_OF_STREAM : 0);
+
+      if (!mSawInputEOS) mExtractor.advance();
+    }
+
+    // decode to PCM and return it
+    int res = mCodec.dequeueOutputBuffer(mMediaCodecInfo, TIMEOUT_US);
+
+    byte[] chunk = new byte[mMediaCodecInfo.size];
+
+    if (res >= 0) {
+      if (mMediaCodecInfo.size > 0)  mNoOutputCounter = 0;
+
+      int outputBufIndex = res;
+      ByteBuffer buf = mCodecOutputBuffers[outputBufIndex];
+      buf.get(chunk);
+      buf.clear();
+
+      if(chunk.length == 0)
+        chunk = null;
+
+      mCodec.releaseOutputBuffer(outputBufIndex, false);
+      if ((mMediaCodecInfo.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+        mSawOutputEOS = true;
+      }
+    }
+
+    return chunk;
+  }
+  public void stopRecordReading(){
+    mCodec.stop();
+    mCodec.release();
+
+    mExtractor = null;
+
+    mCodec = null;
+
+    mCodecInputBuffers = null;
+    mCodecOutputBuffers = null;
+
+    mSawInputEOS = false;
+    mSawOutputEOS = false;
+
+    mPresentationTimeUs = 0;
+
+    mNoOutputCounter = 0;
+    mNoOutputCounterLimit = 0;
+
+    mMediaCodecInfo = null;
+  }
+}
